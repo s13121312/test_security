396957a64260dc51203b79f1e1b28b48bfae0b69
jeongminkang <wjdals980424@naver.com>
2024-09-13 00:28:34 +0900

    feat: 세션 정보 확인

--- src/main/java/com/example/test_security/controller/MainController.java
+++ src/main/java/com/example/test_security/controller/MainController.java
@@ -1,13 +1,36 @@
 package com.example.test_security.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Collection;
+import java.util.Iterator;
 
 @Controller
 public class MainController {
 
     @GetMapping("/")
-    public String mainP() {
+    public String mainP(Model model) {
+
+        //main에다가 로그인했을때 세션정보 출력해주려고 id,
+        String id = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> iter = authorities.iterator();
+        GrantedAuthority auth = iter.next();
+        String role = auth.getAuthority();
+
+        model.addAttribute("id", id);
+        model.addAttribute("role", role);
+
+        //role값을 체크해서 if() admin인지 글삭제 할 수 있게 할 수 있음
+
         return "main";
     }
 }
