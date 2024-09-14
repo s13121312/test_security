de691380661a2439b0df1d4ba41b7b13a1d6a6f4
jeongminkang <wjdals980424@naver.com>
2024-09-15 02:21:03 +0900

    feat: db기반 로그인 검증

---
+++ src/main/java/com/example/test_security/service/CustomUserDetailService.java
@@ -0,0 +1,32 @@
+package com.example.test_security.service;
+
+import com.example.test_security.dto.CustomUserDetails;
+import com.example.test_security.entity.UserEntity;
+import com.example.test_security.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        //DB에서 조회
+        UserEntity userData = userRepository.findByUsername(username);
+        if(userData != null){
+            //UserDetails에 담아서 return하면 AutneticationManager가 검증 함
+            return new CustomUserDetails(userData);
+        }
+
+        return null;
+    }
+}
--- src/main/java/jwt/LoginFilter.java
+++ src/main/java/jwt/LoginFilter.java
@@ -49,12 +49,12 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
     //로그인 성공시 실행하는 메소드 (여기서 JWT를 발급하면 됨)
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
-        super.successfulAuthentication(request, response, chain, authResult);
+        System.out.println("success");
     }
 
     //로그인 실패시 실행하는 메소드
     @Override
     protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException, ServletException {
-        super.unsuccessfulAuthentication(request, response, failed);
+        System.out.print("fail");
     }
 }
