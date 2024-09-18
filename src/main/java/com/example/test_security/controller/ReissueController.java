6406593c0c564661f87ef94df497513ac58dd281
jeongminkang <wjdals980424@naver.com>
2024-09-19 00:22:17 +0900

    feat: refresh rotate(reissue시에 refresh token도 재발급)

--- src/main/java/com/example/test_security/controller/ReissueController.java
+++ src/main/java/com/example/test_security/controller/ReissueController.java
@@ -59,10 +59,24 @@ public class ReissueController {
         String role = jwtUtil.getRole(refresh);
 
         String newAccess = jwtUtil.createJwt("access", username, role, 600000L);
+        String newRefresh = jwtUtil.createJwt("refresh", username, role, 86400000L);
 
         response.setHeader("access", newAccess);
+        response.addCookie(createCookie("refresh", newRefresh));
 
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+
+    private Cookie createCookie(String key, String value) {
+
+        Cookie cookie = new Cookie(key, value);
+        cookie.setMaxAge(24*60*60);
+        //cookie.setSecure(true);
+        //cookie.setPath("/");
+        cookie.setHttpOnly(true);
+
+        return cookie;
     }
 
 }
